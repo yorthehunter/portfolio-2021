4b582ea4bf7c5d50428f00ee70f08d887ddc1922
yorthehunter <boru.design@gmail.com>
2020-04-16 14:01:31 -0400

    adds reset, icons, and a basic structure to work with -- WIP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import style from './app.module.scss'
+import { ReactComponent as Brain } from './icon/brain-in-jar.svg';
+import { ReactComponent as Rocket } from './icon/rocket-ship.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={style.site}>
+      <header className={style.header}>
+        <nav className={style.nav}>
+          <ul>
+            <li>
+              <a href="#About">About</a>
+            </li>
+            <li>
+              <a href="#Work">Work</a>
+            </li>
+            <li>
+              <a href="#Contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <h1 className={style.superHeroHeading}>Brian Cleveland</h1>
+        <ul className={style.heroList}>
+          <li>
+            <Rocket />
+            <h2>
+              Lead Designer, Design Platform at Stitch Fix
+            </h2>
+          </li>
+          <li>
+            <Brain />
+            <h2>
+              10+ years of experience in  design systems, UI engineering, and user-centered product development
+            </h2>
+          </li>
+        </ul>
       </header>
+      <main>
+        <section className={style.skewedBg}>
+          <div className={style.content}>
+            <p>This is my portfolio</p>
+          </div>
+        </section>
+      </main>
+      <footer>
+        <p>The End</p>
+      </footer>
     </div>
   );
 }
